464eb8c432a93ed164b636f4ebfb2b0cb16eb12a
Pavan Yewale <pavanyewale1996@gmail.com>
2025-07-03 11:24:27 +0530

    Test cases added and refactored

--- sql/impls/mysql/parser/condition.go
+++ sql/impls/mysql/parser/condition.go
@@ -43,23 +43,19 @@ func parseCondition(condition *sql.Condition) (string, []any, error) {
 	if condition == nil {
 		return "", nil, nil
 	}
-	var conditionStrings []string = []string{}
-	var conditionValues []any = []any{}
 	switch condition.Operator {
 	case sql.EQ, sql.NEQ, sql.GT, sql.GTE, sql.LT, sql.LTE:
 		if condition.Field == "" {
 			return "", nil, fmt.Errorf("failed to parse condition, error: field is empty")
 		}
-		conditionStrings = append(conditionStrings, fmt.Sprintf("%s %s ?", condition.Field, operatorToStringMap[condition.Operator]))
-		conditionValues = append(conditionValues, condition.Value)
+		return fmt.Sprintf("%s %s ?", condition.Field, operatorToStringMap[condition.Operator]), []any{condition.Value}, nil
 
 	case sql.IN, sql.NOTIN:
 		if condition.Field == "" {
 			return "", nil, fmt.Errorf("failed to parse condition, error: field is empty")
 		}
 		if values, ok := condition.Value.([]any); ok && len(values) > 0 {
-			conditionStrings = append(conditionStrings, fmt.Sprintf("%s %s (%s)", condition.Field, operatorToStringMap[condition.Operator], getPlaceHolders(len(values))))
-			conditionValues = append(conditionValues, values...)
+			return fmt.Sprintf("%s %s (%s)", condition.Field, operatorToStringMap[condition.Operator], getPlaceHolders(len(values))), values, nil
 		} else {
 			return "", nil, fmt.Errorf("failed to parse condition, error: value for IN/NOTIN must be a non-empty slice")
 		}
@@ -70,22 +66,76 @@ func parseCondition(condition *sql.Condition) (string, []any, error) {
 		if value, ok := condition.Value.(string); !ok {
 			return "", nil, fmt.Errorf("failed to parse condition, error: value for LIKE/NOTLIKE must be a string")
 		} else {
-			conditionStrings = append(conditionStrings, fmt.Sprintf("%s %s ?", condition.Field, operatorToStringMap[condition.Operator]))
-			conditionValues = append(conditionValues, value)
+			return fmt.Sprintf("%s %s ?", condition.Field, operatorToStringMap[condition.Operator]), []any{value}, nil
 		}
+
 	case sql.ISNULL, sql.ISNOTNULL:
 		if condition.Field == "" {
 			return "", nil, fmt.Errorf("failed to parse condition, error: field is empty")
 		}
-		conditionStrings = append(conditionStrings, fmt.Sprintf("%s %s", condition.Field, operatorToStringMap[condition.Operator]))
+		return fmt.Sprintf("%s %s", condition.Field, operatorToStringMap[condition.Operator]), nil, nil
 		// ISNULL and ISNOTNULL do not require a value, so we do not append anything to conditionValues
 	case sql.EXISTS, sql.NOTEXISTS:
+		// not implemented in this parser, but can be added later
+		return "", nil, fmt.Errorf("failed to parse condition, error: EXISTS and NOTEXISTS operators are not implemented in this parser")
+	case sql.REGEXP:
 		if condition.Field == "" {
 			return "", nil, fmt.Errorf("failed to parse condition, error: field is empty")
+		}
+		if value, ok := condition.Value.(string); !ok {
+			return "", nil, fmt.Errorf("failed to parse condition, error: value for REGEXP must be a string")
+		} else {
+			return fmt.Sprintf("%s %s ?", condition.Field, operatorToStringMap[condition.Operator]), []any{value}, nil
+		}
+	case sql.AND, sql.OR:
+		if condition.Field != "" {
+			return "", nil, fmt.Errorf("failed to parse condition, error: field should be empty for logical operators: %s", operatorToStringMap[condition.Operator])
+		}
+		if len(condition.Conditions) == 0 {
+			return "", nil, fmt.Errorf("failed to parse condition, error: conditions should not be empty for logical operators: %s", operatorToStringMap[condition.Operator])
+		}
+		var conditionStrings []string
+		var conditionValues []any
+		for _, subCondition := range condition.Conditions {
+			subConditionString, subConditionValues, err := parseCondition(&subCondition)
+			if err != nil {
+				return "", nil, fmt.Errorf("failed to parse sub-condition: %w", err)
+			}
+			if subConditionString != "" {
+				conditionStrings = append(conditionStrings, subConditionString)
+				conditionValues = append(conditionValues, subConditionValues...)
+			}
+		}
+		if len(conditionStrings) == 0 {
+			return "", nil, fmt.Errorf("failed to parse condition, error: no valid sub-conditions found for logical operator: %s", operatorToStringMap[condition.Operator])
+		}
+		return fmt.Sprintf("(%s)", strings.Join(conditionStrings, fmt.Sprintf(" %s ", operatorToStringMap[condition.Operator]))), conditionValues, nil
+	case sql.NOT:
+		if condition.Field != "" {
+			return "", nil, fmt.Errorf("failed to parse condition, error: field should be emptyfor NOT operator")
+		}
+		if len(condition.Conditions) != 1 {
+			return "", nil, fmt.Errorf("failed to parse condition, error: NOT operator should have exactly one sub-condition")
+		}
+		subConditionString, subConditionValues, err := parseCondition(&condition.Conditions[0])
+		if err != nil {
+			return "", nil, fmt.Errorf("failed to parse sub-condition for NOT operator: %w", err)
+		}
+		if subConditionString == "" {
+			return "", nil, fmt.Errorf("failed to parse condition, error: sub-condition for NOT operator should not be empty")
+		}
+		return fmt.Sprintf("NOT (%s)", subConditionString), subConditionValues, nil
+	case sql.BETWEEN, sql.NOTBETWEEN:
+		if condition.Field == "" {
+			return "", nil, fmt.Errorf("failed to parse condition, error: field is empty")
+		}
+		if values, ok := condition.Value.([]any); ok && len(values) == 2 {
+			return fmt.Sprintf("%s %s ? AND ?", condition.Field, operatorToStringMap[condition.Operator]), []any{values[0], values[1]}, nil
+		} else {
+			return "", nil, fmt.Errorf("failed to parse condition, error: value for BETWEEN/NOTBETWEEN must be a slice of two elements")
 		}
 
 	default:
 		return "", nil, fmt.Errorf("failed to parse condition, error: invalid operator: %d, for field: %s", condition.Operator, condition.Field)
 	}
-	return "WHERE " + strings.Join(conditionStrings, " AND "), conditionValues, nil
 }
---
+++ sql/impls/mysql/parser/condition_test.go
@@ -0,0 +1,328 @@
+package parser
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/gofreego/database/sql"
+)
+
+func Test_parseCondition(t *testing.T) {
+	type args struct {
+		condition *sql.Condition
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		want1   []any
+		wantErr bool
+	}{
+		{
+			name: "test with nil condition",
+			args: args{
+				condition: nil,
+			},
+			want:    "",
+			want1:   nil,
+			wantErr: false,
+		},
+		{
+			name: "test with simple condition",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "name",
+					Value:    "John",
+					Operator: sql.EQ,
+				},
+			},
+			want:    "name = ?",
+			want1:   []any{"John"},
+			wantErr: false,
+		},
+		{
+			name: "test with IN condition",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "id",
+					Value:    []any{1, 2, 3},
+					Operator: sql.IN,
+				},
+			},
+			want:    "id IN (?, ?, ?)",
+			want1:   []any{1, 2, 3},
+			wantErr: false,
+		},
+		{
+			name: "test with empty IN condition",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "id",
+					Value:    []any{},
+					Operator: sql.IN,
+				},
+			},
+			want:    "",
+			want1:   nil,
+			wantErr: true,
+		},
+		{
+			name: "test with NOT IN condition",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "id",
+					Value:    []any{1, 2, 3},
+					Operator: sql.NOTIN,
+				},
+			},
+			want:    "id NOT IN (?, ?, ?)",
+			want1:   []any{1, 2, 3},
+			wantErr: false,
+		},
+		{
+			name: "test with LIKE condition",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "name",
+					Value:    "John%",
+					Operator: sql.LIKE,
+				},
+			},
+			want:    "name LIKE ?",
+			want1:   []any{"John%"},
+			wantErr: false,
+		},
+		{
+			name: "test with NOT LIKE condition",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "name",
+					Value:    "John%",
+					Operator: sql.NOTLIKE,
+				},
+			},
+			want:    "name NOT LIKE ?",
+			want1:   []any{"John%"},
+			wantErr: false,
+		},
+		{
+			name: "test with IS NULL condition",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "name",
+					Operator: sql.ISNULL,
+				},
+			},
+			want:    "name IS NULL",
+			want1:   nil,
+			wantErr: false,
+		},
+		{
+			name: "test with IS NOT NULL condition",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "name",
+					Operator: sql.ISNOTNULL,
+				},
+			},
+			want:    "name IS NOT NULL",
+			want1:   nil,
+			wantErr: false,
+		},
+		{
+			name: "test with EXISTS condition",
+			args: args{
+				condition: &sql.Condition{
+					Operator: sql.EXISTS,
+					Conditions: []sql.Condition{
+						{
+							Field:    "id",
+							Value:    1,
+							Operator: sql.EQ,
+						},
+					},
+				},
+			},
+			want:    "",
+			want1:   nil,
+			wantErr: true, // EXISTS is not implemented in this parser
+		},
+		{
+			name: "test with NOT EXISTS condition",
+			args: args{
+				condition: &sql.Condition{
+					Operator: sql.NOTEXISTS,
+					Conditions: []sql.Condition{
+						{
+							Field:    "id",
+							Value:    1,
+							Operator: sql.EQ,
+						},
+					},
+				},
+			},
+			want:    "",
+			want1:   nil,
+			wantErr: true, // NOT EXISTS is not implemented in this parser
+		},
+		{
+			name: "test with REGEXP condition",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "name",
+					Value:    "^[A-Z].*",
+					Operator: sql.REGEXP,
+				},
+			},
+			want:    "name REGEXP ?",
+			want1:   []any{"^[A-Z].*"},
+			wantErr: false,
+		},
+		{
+			name: "test with BETWEEN condition",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "age",
+					Value:    []any{18, 30},
+					Operator: sql.BETWEEN,
+				},
+			},
+			want:    "age BETWEEN ? AND ?",
+			want1:   []any{18, 30},
+			wantErr: false,
+		},
+		{
+			name: "test with NOT BETWEEN condition",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "age",
+					Value:    []any{18, 30},
+					Operator: sql.NOTBETWEEN,
+				},
+			},
+			want:    "age NOT BETWEEN ? AND ?",
+			want1:   []any{18, 30},
+			wantErr: false,
+		},
+		{
+			name: "test with AND condition",
+			args: args{
+				condition: &sql.Condition{
+					Operator: sql.AND,
+					Conditions: []sql.Condition{
+						{
+							Field:    "name",
+							Value:    "John",
+							Operator: sql.EQ,
+						},
+						{
+							Field:    "age",
+							Value:    30,
+							Operator: sql.GT,
+						},
+					},
+				},
+			},
+			want:    "(name = ? AND age > ?)",
+			want1:   []any{"John", 30},
+			wantErr: false,
+		},
+		{
+			name: "test with OR condition",
+			args: args{
+				condition: &sql.Condition{
+					Operator: sql.OR,
+					Conditions: []sql.Condition{
+						{
+
+							Field:    "name",
+							Value:    "John",
+							Operator: sql.EQ,
+						},
+						{
+							Field:    "age",
+							Value:    30,
+							Operator: sql.GT,
+						},
+					},
+				},
+			},
+			want:    "(name = ? OR age > ?)",
+			want1:   []any{"John", 30},
+			wantErr: false,
+		},
+		{
+			name: "test with NOT condition",
+			args: args{
+				condition: &sql.Condition{
+					Operator: sql.NOT,
+					Conditions: []sql.Condition{
+						{
+							Field:    "name",
+							Value:    "John",
+							Operator: sql.EQ,
+						},
+					},
+				},
+			},
+			want:    "NOT (name = ?)",
+			want1:   []any{"John"},
+			wantErr: false,
+		},
+		{
+			name: "test with invalid operator",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "name",
+					Value:    "John",
+					Operator: sql.Operator(999), // Invalid operator
+				},
+			},
+			want:    "",
+			want1:   nil,
+			wantErr: true,
+		},
+		{
+			name: "test with empty field",
+			args: args{
+				condition: &sql.Condition{
+					Field:    "",
+					Value:    "John",
+					Operator: sql.EQ,
+				},
+			},
+			want:    "",
+			want1:   nil,
+			wantErr: true,
+		},
+		{
+			name: "test with empty value for IN condition",
+
+			args: args{
+				condition: &sql.Condition{
+					Field:    "id",
+					Value:    []any{},
+					Operator: sql.IN,
+				},
+			},
+			want:    "",
+			want1:   nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := parseCondition(tt.args.condition)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseCondition() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("parseCondition() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("parseCondition() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
