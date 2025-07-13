3a0585242b3835940a8689975facb08836da460d
Pavan Yewale <pavanyewale1996@gmail.com>
2025-07-13 23:12:55 +0530

    Add comprehensive test cases for UpdateQuery parsing in MySQL parser; enhance parseUpdates function to improve error handling and query generation; rename updateQuery to updateClause for clarity.

--- sql/impls/mysql/parser/update.go
+++ sql/impls/mysql/parser/update.go
@@ -12,10 +12,10 @@ const (
 )
 
 func parseUpdates(updates *sql.Updates) (string, []int, error) {
-	var updateQuery string
+	var updateClause string
 	var valueIndexes []int
 
-	for _, update := range updates.Fields {
+	for i, update := range updates.Fields {
 		if update.Field == "" {
 			return "", nil, errors.New("field is empty for update")
 		}
@@ -23,25 +23,30 @@ func parseUpdates(updates *sql.Updates) (string, []int, error) {
 			return "", nil, errors.New("value is nil for update field: " + update.Field)
 		}
 
+		// Add comma separator if not the first field
+		if i > 0 {
+			updateClause += ", "
+		}
+
 		if update.Value.IsColumn() {
 			if update.Value.Value == nil || update.Value.Value == "" {
 				return "", nil, errors.New("value is empty for update field: " + update.Field + " and value type is a column")
 			}
-			updateQuery += fmt.Sprintf("%s = %s", update.Field, update.Value.Value)
+			updateClause += fmt.Sprintf("%s = %s", update.Field, update.Value.Value)
 		} else if update.Value.Value != nil {
-			updateQuery += fmt.Sprintf("%s = %s", update.Field, getValue(update.Value.Value))
+			updateClause += fmt.Sprintf("%s = %s", update.Field, getValue(update.Value.Value))
 		} else {
-			updateQuery += fmt.Sprintf("%s = ?", update.Field)
+			updateClause += fmt.Sprintf("%s = ?", update.Field)
 			valueIndexes = append(valueIndexes, update.Value.Index)
 		}
 	}
 
-	return updateQuery, valueIndexes, nil
+	return updateClause, valueIndexes, nil
 }
 
 func ParseUpdateQuery(table *sql.Table, updates *sql.Updates, condition *sql.Condition) (string, []int, error) {
 	var valueIndexes []int
-	var updateQuery string
+	var updateClause string
 	var err error
 
 	if updates == nil {
@@ -51,16 +56,16 @@ func ParseUpdateQuery(table *sql.Table, updates *sql.Updates, condition *sql.Con
 	if err != nil {
 		return "", nil, err
 	}
+	updateClause, updateValueIndexes, err := parseUpdates(updates)
+	if err != nil {
+		return "", nil, err
+	}
+	valueIndexes = append(valueIndexes, updateValueIndexes...)
 	conditionQuery, conditionValueIndexes, err := parseCondition(condition)
 	if err != nil {
 		return "", nil, err
 	}
 	valueIndexes = append(valueIndexes, conditionValueIndexes...)
-	updateQuery, updateValueIndexes, err := parseUpdates(updates)
-	if err != nil {
-		return "", nil, err
-	}
-	valueIndexes = append(valueIndexes, updateValueIndexes...)
 
-	return fmt.Sprintf(updateQuery, tableName, updateQuery, conditionQuery), valueIndexes, nil
+	return fmt.Sprintf(updateQuery, tableName, updateClause, conditionQuery), valueIndexes, nil
 }
